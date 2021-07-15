61e00ec7b848a301ae9a356231ceaccd8a7448df
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-07-15 19:24:17 +0200

    Working on get books

--- backend_php/addBook.php
+++ backend_php/addBook.php
@@ -6,8 +6,17 @@
     $price = $_POST['price'];
 
     $db = mysqli_connect("localhost", "root", "password", "bookbase"); //Server host, username, password and database name credentials.
-    $query = "INSERT INTO book (book_name, author, isbn, publisher, price)
-            VALUES ('$bookName', '$author', '$isbn_number', '$publisher', '$price')";
+    $query = "INSERT INTO books (book_name, author, isbn, publisher, price) 
+            VALUES ('$bookName', '$author', '$isbn_number', '$publisher', '$price')"; //Inserting the form data into the database
 
-    mysqli_query($db, $query);
+    mysqli_query($db, $query); //Executing the insertion query
+
+    $query = "SELECT id FROM books WHERE publisher = '$publisher'";
+    $result = mysqli_query($db, $query);
+
+    $row = mysqli_fetch_array($result);
+    echo "id is: " . $row[0];
+
+    header('Location: http://localhost/BookBase_PHP/frontend/createBook.html')  //go to another page on error
+    
 ?>
